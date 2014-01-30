d2b239174fdffb77406702a8b2b6de9e8b4af2a0
thorkill <thkr@hannover.ccc.de>
2014-01-30 17:47:19 +0100

    added new keybindings

--- dot.conkerorrc/thorkill-keybinding.js
+++ dot.conkerorrc/thorkill-keybinding.js
@@ -1,5 +1,6 @@
 
-
+// quick content-policy js whitelist/blacklist
+define_key(content_buffer_normal_keymap, "f10", "cp-js-show");
 // quick content-policy-toggle
 define_key(content_buffer_normal_keymap, "f12", "content-policy-toggle");
 define_key(content_buffer_normal_keymap, "C-f", "follow-new-buffer-background");
