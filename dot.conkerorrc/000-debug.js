d5102aa4da007b2169927f10dfd50d900fcbbacb
thorkill <thkr@hannover.ccc.de>
2015-12-25 17:42:55 +0100

    Extended dump functions

--- dot.conkerorrc/000-debug.js
+++ dot.conkerorrc/000-debug.js
@@ -1,12 +1,26 @@
 
+
 function jsdump(str) {
-  Components.classes['@mozilla.org/consoleservice;1']
-            .getService(Components.interfaces.nsIConsoleService)
-            .logStringMessage(str);
+    dumpln(str);
+    //Components.classes['@mozilla.org/consoleservice;1']
+    //      .getService(Components.interfaces.nsIConsoleService)
+    //    .logStringMessage(str);
+}
+
+function _simple_obj_dump(obj) {
+    for(var i in obj) {
+        jsdump(i + " / " + typeof(obj[i]));
+    }
 }
 
 function _dump_obj(obj) {
+    jsdump("------ dump start ------");
     for (var i in obj) {
-        jsdump(i + ' : ' + obj[i]+"\n");
+        try {
+            jsdump(i + ' : ' + obj[i]+"\n");
+        } catch (e) {
+            jsdump("Error for: i=" + i + "/" + e);
+        }
     }
+    jsdump("------ dump end   ------");
 }
